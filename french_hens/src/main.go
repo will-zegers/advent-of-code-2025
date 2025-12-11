4c398eec2ffac6257ba7ac613ee8a95cb2d7e4af
Will Zegers <will.zegers@gmail.com>
2025-12-11 15:22:29 -0800

    Day 3 part 2 (and rework for part 1)

--- french_hens/src/main.go
+++ french_hens/src/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 )
 
 func main() {
@@ -17,37 +18,60 @@ func main() {
 	scanner := bufio.NewScanner(input)
 	max_joltage := 0
 	for scanner.Scan() {
+		var content []int
 		line := scanner.Bytes()
-		result := compute_maximum_joltage(line)
+		for _, elem := range line {
+			content = append(content, int(elem-'0'))
+		}
+
+		result := compute_maximum_joltage(content, 12)
 		max_joltage += result
 	}
 
 	fmt.Println(max_joltage)
 }
 
-func compute_maximum_joltage(content []byte) int {
-	joltages := []JoltagePair{JoltagePair{0, 0}}
-
-	for i := 0; i < len(content); i++ {
-		tens := joltages[i].tens
-		ones := joltages[i].ones
-		next := int(content[i] - '0')
-
-		var next_pair JoltagePair
-		if combined_value(ones, next) > combined_value(tens, ones) {
-			next_pair = JoltagePair{ones, next}
-		} else if combined_value(tens, next) > combined_value(tens, ones) {
-			next_pair = JoltagePair{tens, next}
-		} else {
-			next_pair = joltages[i]
-		}
-
-		joltages = append(joltages, next_pair)
+func compute_maximum_joltage(content []int, n int) int {
+	// Create a slice of candidate banks, consisting of the first 'n'
+	// of the input
+	var banks []int
+	for i := range n {
+		banks = append(banks, content[i])
 	}
 
-	return joltages[len(joltages)-1].Value()
+	/* Look through the remaining banks in our input and, as we add new
+	ones to the end, we pop off the first number that is lower than
+	its next neighor.
+
+	e.g. If n = 3, our current banks are 423, and the next number is
+	7, we first pop out the '2' so our new higher number is 437
+	*/
+	for i := n; i < len(content); i++ {
+		for j := range n - 1 {
+			if banks[j] < banks[j+1] {
+				banks = slices.Delete(banks, j, j+1)
+				banks = append(banks, content[i])
+				break // We've found a spot for our new number, so move onto the next
+			}
+		}
+
+		// If we make it to this point, then no changes were made to the
+		// list of candidate banks. Instead now just see if we need to swap
+		// out a new higher number for the one at the end of the list
+		if banks[len(banks)-1] < content[i] {
+			banks[len(banks)-1] = content[i]
+		}
+	}
+	return accumulate_joltages(banks)
 }
 
-func combined_value(tens int, ones int) int {
-	return (tens-'0')*10 + ones - '0'
+// Helper function to put the slice of bank joltages into an int
+// of their correct decimal notation
+func accumulate_joltages(banks []int) int {
+	accum := 0
+	for _, joltage := range banks {
+		accum = accum*10 + joltage
+	}
+
+	return accum
 }
