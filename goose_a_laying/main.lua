70cac8fe3eb9f2adfb77b4390d6dbd51365e5de4
Will Zegers <will.zegers@gmail.com>
2025-12-14 22:55:31 -0800

    Day 6 part 2

--- goose_a_laying/main.lua
+++ goose_a_laying/main.lua
@@ -1,4 +1,6 @@
-function main () local i = 1
+function main ()
+
+  local i = 1
   local store = {}
   for line in io.lines("input.txt") do
     store[i] = line
@@ -6,30 +8,76 @@ function main () local i = 1
   end
 
   local operands = {}
+  local operandsStr = {}
   local operators  = {}
+  local operatorIndices = {}
 
-  -- Begin by creating nested tables in 'operands' to hold column-wise
-  -- values based on how many values we expect to see in each row
+  -- Three-for-one: extract individual operators, their index in the string
+  -- (which will give us column width calculations), and initialize the operand
+  -- table to hold nested column-wise tables of numbers
   local i = 1
-  for digit_str in string.gmatch(store[1], "%d+") do
-    operands[i] = {}
-    i = i + 1
-  end
+  local operatorIdx = 1
+  operatorRowStr = store[#store]
+  while operatorIdx do -- while we're still finding operators (not nil)
+    operandsStr[i] = {}
+    operatorIndices[i] = operatorIdx
+    operators[i] = operatorRowStr:sub(operatorIdx, operatorIdx)
 
-  -- Populate the individual column-wise tables
-  for j = 1, #store - 1 do
-    i = 1
-    for digit_str in string.gmatch(store[j], "%d+") do
-      operands[i][j] = tonumber(digit_str)
+    local nextIdx = operatorRowStr:find("[%*|%+]", operatorIdx+1)
+    if nextIdx then
+      operatorIdx = nextIdx
       i = i + 1
+    else
+      -- Add a sort of "sentinel" index in order to calculate the width of the last row
+      operatorIndices[i+1] = #operatorRowStr + 2
+      break;
     end
   end
 
-  -- Extract our operators
-  local operatorRow = store[#store]
-  local squishedOpRow = operatorRow:gsub("%s", "") -- whitespace cleanup
-  for i=1, #squishedOpRow do
-    operators[i] = string.sub(squishedOpRow, i, i)
+  -- Populate the individual column-wise tables with raw string values (and
+  -- whatever whitespace padding. It'll be usefull ;) )
+  for j = 1, #store - 1 do
+    for i = 1, #operandsStr do
+      operandsStr[i][j] = store[j]:sub(operatorIndices[i], operatorIndices[i+1] - 2)
+    end
+  end
+
+  -- TODO: There's gotta be a better way to create nested zeroed-out tables via
+  -- metatables and default values or something?, so this loop would be unnecessary.
+  -- I'm coming off 36 hours having ever since touched Lua. Maybe somehow do it in
+  -- that "three-for-one" above?
+  for i = 1, #operandsStr do
+    operands[i] = {}
+    for j = 1, #store - 1 do
+      for k = 1, #operandsStr[i][j] do
+        operands[i][k] = 0
+      end
+    end
+  end
+
+  -- Now the fun part! Some dimensional transformation to go from left-to right,
+  -- top-to-bottom human math to top-to-bottom, right-to-left cephalopod math, e.g.
+  --
+  --        Human:|   Cephalopod:
+  --         1→2→3|       1  2  3
+  --              |          ↓  ↓
+  --           4→5|          4  5
+  --              |             ↓
+  --             6|             6
+  --         +    |       +
+  --         ------       -------
+  -- (123 + 45 +6) (356 + 24 + 1)
+  --
+  -- (at least '+' and '*' are commutative, so operand order doesn't matter)
+  for i = 1, #operandsStr do
+    for j = 1, #store - 1 do
+      for k = 1, #operandsStr[i][j] do
+        number = operandsStr[i][j]:sub(k, k)
+        if number ~= ' ' then -- and that's why we kept the whitespaces in the string
+          operands[i][k] = operands[i][k] * 10 + number
+        end
+      end
+    end
   end
 
   local total = 0
@@ -37,7 +85,7 @@ function main () local i = 1
     total = total + reduce(operands[i], operators[i])
   end
 
-  print(total)
+  print(total) -- retrospect: hfs I can't believe that actually worked!
 end
 
 function reduce (operands, operator)
